3920052cd4acb137c096a063836efb9d8943d40b
Anderson Nunes <anderson.nunes1988@gmail.com>
2018-07-04 12:47:40 -0300

    SportsStore: Modifying and Deleting Data

--- SportsStore/SportsStore/Controllers/HomeController.cs
+++ SportsStore/SportsStore/Controllers/HomeController.cs
@@ -9,13 +9,40 @@ namespace SportsStore.Controllers {
         public HomeController(IRepository repo) => repository = repo;
 
         public IActionResult Index() {
-            System.Console.Clear();
+            //System.Console.Clear();
             return View(repository.Products);
         }
 
         [HttpPost]
         public IActionResult AddProduct(Product product) {
             repository.AddProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UpdateProduct(long key) {
+            return View(repository.GetProduct(key));
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product) {
+            repository.UpdateProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UpdateAll() {
+            ViewBag.UpdateAll = true;
+            return View(nameof(Index), repository.Products);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAll(Product[] products) {
+            repository.UpdateAll(products);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product) {
+            repository.Delete(product);
             return RedirectToAction(nameof(Index));
         }
     }
